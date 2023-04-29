9ddc6fb196c502e551f3f2f5ec341b94f9327cde
Adrieto <parodiadrian3@gmail.com>
2023-04-29 12:58:34 -0300

    29/4/23

--- Data Structures and Algorithms/Curso UTN/TP1_patentes.py
+++ Data Structures and Algorithms/Curso UTN/TP1_patentes.py
@@ -1,4 +1,4 @@
-# Pruebas para el TP 1 de las patentes
+# TP1 Patentes y peajes
 # Formatos:
 
 # Argentina: LLNNNLL
@@ -7,27 +7,29 @@
 # Paraguay:  LLLLNNN
 # Uruguay:   LLLNNNN
 
-
 # patente = "AK348LZ" # Arg
 # patente = "AP85986" # Bol
 # patente = "ASF5Q99" # Bra
 # patente = "ASFQ849" # Par
-patente = "ASF8249"  # Uru
+# patente = "ASF8492"  # Uru
+patente = "1" # Otro
 
-if patente[0:2].isalpha() and patente[2:5].isdecimal() and patente[5:7].isalpha():
+# Chequeamos que el origen de las patentes:
+if len(patente) == 7 and patente[0:2].isalpha() and patente[2:5].isdecimal() and patente[5:7].isalpha():
     pais_procedencia = "Argentina"
-elif patente[0:2].isalpha() and patente[2:7].isdecimal():
+elif len(patente) == 7 and patente[0:2].isalpha() and patente[2:7].isdecimal():
     pais_procedencia = "Bolivia"
 elif (
-    patente[0:3].isalpha()
+    len(patente) == 7
+    and patente[0:3].isalpha()
     and patente[3].isdecimal()
     and patente[4].isalpha()
     and patente[5:7].isdecimal()
 ):
     pais_procedencia = "Brasil"
-elif patente[0:4].isalpha() and patente[4:7].isdecimal():
+elif len(patente) == 7 and patente[0:4].isalpha() and patente[4:7].isdecimal():
     pais_procedencia = "Paraguay"
-elif patente[0:3].isalpha() and patente[3:7].isdecimal():
+elif len(patente) == 7 and patente[0:3].isalpha() and patente[3:7].isdecimal():
     pais_procedencia = "Uruguay"
 else:
     pais_procedencia = "Otro"
